67f884f4eed1c6699f2d71b9b128ef51e7ad3ab1
Dev Sonone <devesonone222@gmail.com>
2026-03-15 12:00:11 +0530

    Updated code

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -51,7 +51,7 @@ const Dashboard = () => {
 
     fetchData();
 
-    const interval = setInterval(fetchData, 5000);
+    const interval = setInterval(fetchData, 3000);
      return () => clearInterval(interval);
 
   }, [navigate]);
